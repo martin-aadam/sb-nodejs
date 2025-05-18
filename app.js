90fca838407a8eee530385097cf255c05660bb0f
martin-aadam <martin.adam@gmx.us>
2025-05-19 01:43:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -66,20 +66,37 @@ async function main() {
         } else if (req.url === `/${UUID}`) {
             let vlessURL;
             if (NAME.includes('server') || NAME.includes('hostypanel')) {
-            vlessURL = `vless://${UUID}@${DOMAIN}:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@usa.visa.com:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@www.speedtest.net:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@vless.cloudflare.182682.xyz:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@speed.marisalnc.com:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@xn--b6gac.eu.org:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@freeyx.cloudflare88.eu.org:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@cf.090227.xyz:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
+            vlessURL = `vless://${UUID}@${DOMAIN}:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#${DOMAIN}
 vless://${UUID}@104.24.0.0:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
 vless://${UUID}@104.25.0.0:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@104.26.0.0:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
-vless://${UUID}@104.27.0.0:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
 vless://${UUID}@[2606:4700::]:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
 vless://${UUID}@[2400:cb00:2049::]:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}
+vless://${UUID}@usa.visa.com:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#usa.visa.com
+vless://${UUID}@www.speedtest.net:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#www.speedtest.net
+vless://${UUID}@vless.cloudflare.182682.xyz:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#vless.cloudflare.182682.xyz
+vless://${UUID}@xn--b6gac.eu.org:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#xn--b6gac.eu.org
+vless://${UUID}@freeyx.cloudflare88.eu.org:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#freeyx.cloudflare88.eu.org
+vless://${UUID}@cf.090227.xyz:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#cf.090227.xyz
+vless://${UUID}@104.21.20.170:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#西雅图1
+vless://${UUID}@[2606:4700:3032:5b27:ce4b:4cc:c06e:3175]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#西雅图2
+vless://${UUID}@[2606:4700:2f:f708:4039:37a3:ff7a:b79]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#西雅图3
+vless://${UUID}@[2606:4700:2a:db89:3545:d747:280e:a4ed]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#西雅图4
+vless://${UUID}@[2606:4700:a9:268f:2b1b:d739:d86d:873b]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#圣何塞1
+vless://${UUID}@[2606:4700:a9:2a14:e6d:a90c:e54c:bdda]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#圣何塞2
+vless://${UUID}@[2606:4700:4406:bdd2:3e80:4eba:7fc7:c155]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#洛杉矶1
+vless://${UUID}@[2606:4700:8:bc63:62d6:2a10:689c:a751]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#洛杉矶2
+vless://${UUID}@[2606:4700:440a:50cb:6d57:59c5:fbb0:dcfa]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#洛杉矶3
+vless://${UUID}@[2606:4700:85d1:8fe4:9257:e3c8:b89d:e6d2]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#法兰克福1
+vless://${UUID}@[2606:4700:8d7a:5772:aa01:129e:cccb:1c24]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#法兰克福2
+vless://${UUID}@[2606:4700:8d96:1cdc:b51f:4a0e:17d6:9f3b]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#法兰克福3
+vless://${UUID}@[2606:4700:9c6b:28ce:fbd4:970d:7533:9342]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#香港1
+vless://${UUID}@[2606:4700:8399:cdd3:122b:4f66:5071:9bf6]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#香港2
+vless://${UUID}@[2606:4700:9a62:76bb:ab4a:6521:2713:d528]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#香港3
+vless://${UUID}@[2606:4700:83b9:2ccd:f58e:1909:b6a7:d74f]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#印度1
+vless://${UUID}@[2606:4700:83b4:2e2f:b4b0:a1b6:b715:5c1d]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#印度2
+vless://${UUID}@[2606:4700:83b5:f69:df86:a4f1:9cdb:cd77]:8443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#印度3
+
+
 `;
         } else {
             vlessURL = `vless://${UUID}@${DOMAIN}:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#Vl-ws-tls-${NAME}`;
